c093e90cf12efd0a425b53a989a4fb630a6cd8cc
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-04-23 15:04:08 +0430

    read n and b part added

---
+++ main.py
@@ -0,0 +1,24 @@
+from numpy import *
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    
+    # give matrix is a square matrix
+    n_matrix, number_of_b = input().split(' ')
+    n_matrix, number_of_b = int(n_matrix), int(number_of_b)
+
+
+
+    
+
