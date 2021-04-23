712f6416ce5b46800c94098ba080ecd6e13a5441
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2021-04-23 16:21:14 +0430

    calculate_LU fucntion added

--- main.py
+++ main.py
@@ -4,6 +4,33 @@ from numpy import *
 
 
 
+def calculate_LU(matrix: list) -> tuple:
+
+    n_matrix = len(matrix)
+
+    U = matrix
+    L = [[(1 if i == j else 0) for j in range(n_matrix)] for i in range(n_matrix)]
+
+    
+    current_pivot = 0
+    for i in range(n_matrix):
+        for j in range(i+1, n_matrix):
+
+            operator = -1 * (matrix[j][current_pivot]/matrix[i][current_pivot])
+            L[j][current_pivot] = -operator
+
+            for x in range(current_pivot, n_matrix):
+                U[j][x] += operator * U[i][x]
+
+        current_pivot += 1                
+
+
+    print(U)
+    print('\n', L)
+
+    return ()
+
+
 
 
 
@@ -27,3 +54,4 @@ if __name__ == "__main__":
 
 
     
+
